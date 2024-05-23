10a8ebbf5e678cba315b9a0e22b42e750f47d04c
macie <hadut3@gmail.com>
2024-05-23 13:01:25 +0200

    add godot test project into git

---
+++ smth/Scripts/slime.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+const speed = 60
+var direction = 1
+@onready var ray_cast_left = $RayCastLeft
+@onready var ray_cast_right = $RayCastRight
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	elif ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	position.x += speed * delta * direction
