572db8074890b054fd5aad22e7e6dda8c9b64509
macie <hadut3@gmail.com>
2024-06-11 16:48:29 +0200

    add movement in y axis

--- smth/Scripts/player.gd
+++ smth/Scripts/player.gd
@@ -7,7 +7,10 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var collision_shape = $CollisionShape2D
 
+var mouse_pos
+var facing = 1
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,27 +20,50 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	
+#	if Input.is_action_pressed("ui_right"):
+#		velocity.x += 1
+#	if Input.is_action_pressed("ui_left"):
+#		velocity.x -= 1
+#	if Input.is_action_pressed("ui_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("ui_up"):
+#		velocity.y -= 1
+	
+	var directionX = Input.get_axis("move_left", "move_right")
+	var directionY = Input.get_axis("move_up", "move_down")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	
-	if direction > 0:
+	if directionX > 0:
 		animated_sprite.flip_h = false
-	elif direction < 0:
+	elif directionX < 0:
 		animated_sprite.flip_h = true
-	
+
 	if is_on_floor():
-		if direction == 0:
+		if directionX == 0 and directionY == 0:
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
-	
-	if direction:
-		velocity.x = direction * SPEED
+
+	if directionX:
+		velocity.x = directionX * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if directionY:
+		velocity.y = directionY * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
+	
+#	if directionX != 0:
+#		facing = directionX
+#	var pos = global_position
+#	pos.x += facing * 16
+#	var space_state = get_world_2d().direct_space_state
+#	var query = PhysicsRayQueryParameters2D.create(global_position, pos,
+#		collision_mask, [self])
+#	var result = space_state.intersect_ray(query)
+#	if result:
+#		print("Hit at point: ", result.collider)
